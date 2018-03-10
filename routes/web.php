ba85726982650776437cdf2b8b4c2316a63b2fd8
waleed naveed <valeednaveed@gmail.com>
2018-03-11 04:09:59 +0500

    i

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@
 //     return view('welcome');
 // });
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('mainPage');
 Route::get('/portfolio', 'HomeController@index')->name('portfolio');
 
 Route::post('/', 'HomeController@storeComment');
