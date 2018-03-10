213a7cbed767c7847d19fd13ffed685485363efb
waleed naveed <valeednaveed@gmail.com>
2018-03-11 04:04:23 +0500

    database

--- config/database.php
+++ config/database.php
@@ -2,6 +2,12 @@
 
 <?php
 
+$url = parse_url(getenv("DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 
 
 return [
@@ -17,7 +23,7 @@ return [
 |
 */
 
-'default' => env('DB_CONNECTION', 'mysql'),
+'default' => env('DB_CONNECTION', 'pgsql'),
 
 /*
 |--------------------------------------------------------------------------
@@ -59,16 +65,16 @@ return [
 ],
 
 
-'pgsql' => [
-'driver' => 'pgsql',
-'host' => '',
-'database' =>' $database',
-'username' =>' $username',
-'password' => '$password',
-'charset' => 'utf8',
-'prefix' => '',
-'schema' => 'public',
-],
+    'pgsql' => array(
+        'driver'   => 'pgsql',
+        'host'     => $host,
+        'database' => $database,
+        'username' => $username,
+        'password' => $password,
+        'charset'  => 'utf8',
+        'prefix'   => '',
+        'schema'   => 'public',
+    ),
 'sqlsrv' => [
 'driver' => 'sqlsrv',
 'host' => env('DB_HOST', 'localhost'),
